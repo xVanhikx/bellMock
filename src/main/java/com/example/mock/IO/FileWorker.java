47fe100d631b3ef3eef655f688de6fd8e3520797
Vanhik <Vanhik97@yandex.ru>
2025-04-03 13:52:56 +0300

    123

--- src/main/java/com/example/mock/IO/FileWorker.java
+++ src/main/java/com/example/mock/IO/FileWorker.java
@@ -1,28 +1,27 @@
 package com.example.mock.IO;
 
 import com.example.mock.model.User;
+import org.springframework.stereotype.Component;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Random;
 
+@Component
 public class FileWorker {
     private static final String OUTPUT_FILE = "entities.txt";
     private static final String RANDOM_DATA_FILE = "random_data.txt";
     private static final Random random = new Random();
 
-    public static void writeEntityToFile(User user) {
-        String json = "{\n" +
-                "\"login\":\"" + user.getLogin() + "\",\n" +
-                "\"password\":\"" + user.getPassword() + "\",\n" +
-                "\"email\":\"" + user.getEmail() + "\",\n" +
-                "\"registrationDate\":\"" + user.getRegistrationDate() + "\"\n" +
+    public void writeEntityToFile(User user) {
+        String json = "{" +
+                "\"login\":\"" + user.getLogin() + "\"," +
+                "\"password\":\"" + user.getPassword() + "\"," +
+                "\"email\":\"" + user.getEmail() + "\"," +
+                "\"registrationDate\":\"" + user.getRegistrationDate() + "\"" + ///
                 "}";
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(OUTPUT_FILE, true))) {
             writer.write(json);
@@ -32,21 +31,16 @@ public class FileWorker {
         }
     }
 
-    public static void generateRandomDataFile() {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(RANDOM_DATA_FILE))) {
-            for (int i = 0; i < 5; i++) {
-                writer.write("login: user" + random.nextInt(1000));
-                writer.newLine();
-                writer.write("password: pass" + random.nextInt(9999));
-                writer.newLine();
+    public String readRandomLineFromFile() throws IOException {
+        String str = null;
+        try (BufferedReader reader = new BufferedReader(new FileReader(RANDOM_DATA_FILE))) {
+            for (int i = 0; i <= (random.nextInt(10) + 1); i++) {
+                str = reader.readLine();
             }
+
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
-    }
-
-    public static String readRandomLineFromFile() throws IOException {
-        List<String> lines = Files.readAllLines(Paths.get(RANDOM_DATA_FILE));
-        return lines.isEmpty() ? null : lines.get(random.nextInt(lines.size()));
+        return str;
     }
 }
--- src/main/java/com/example/mock/controller/Controller.java
+++ src/main/java/com/example/mock/controller/Controller.java
@@ -4,33 +4,29 @@ import com.example.mock.IO.FileWorker;
 import com.example.mock.connection.DataBaseWorker;
 import com.example.mock.exceptions.UserNotFoundException;
 import com.example.mock.model.User;
-import io.micrometer.core.instrument.MeterRegistry;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 @RestController
 @RequestMapping("/api")
 public class Controller {
     @Autowired
     private DataBaseWorker dbWorker;
-    static {
-        FileWorker.generateRandomDataFile();
-    }
-    @GetMapping("/status")
+    @Autowired
+    FileWorker fileWorker;
+
+
+    @GetMapping("/status/{login}")
     public ResponseEntity<String> getStatus(@PathVariable String login) {
         timer(100);
         try {
             User user = dbWorker.getUserByLogin(login);
-            FileWorker.writeEntityToFile(user);
+            fileWorker.writeEntityToFile(user);
             return ResponseEntity.ok(user.toString());
         } catch (UserNotFoundException e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage()); //
@@ -54,7 +50,7 @@ public class Controller {
     public String getRandomLine() {
         String str = null;
         try {
-            str = FileWorker.readRandomLineFromFile();
+            str = fileWorker.readRandomLineFromFile();
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
